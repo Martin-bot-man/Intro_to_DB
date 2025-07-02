f937b8587f1e60d5e26dbc44e89a4ca8a3c46ef3
Martin Odongo <martinwere14@gmail.com>
2025-07-02 21:07:30 +0300

    library database

---
+++ alx_book_store.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Authors(
+    id INT book_id(PRIMARY KEY),
+    title VARCHAR(130) NOT NULL,
+    FOREIGN KEY (book_id) REFERENCES Authors(id)
+);
+
+CREATE TABLE Authors(
+    id INT author_id(PRIMARY KEY),
+    author_name VARCHAR(215) NOT NULL,
+);
+CREATE Customers(
+    id INT customer_id(PRIMARY KEY),
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
+    address TEXT NOT NULL,
+);
+CREATE Orders(
+    id INT order_id(PRIMARY KEY),
+    FOREIGN KEY (customer_id) REFERENCES Customers(id),
+    order_date DATE,
+);
+CREATE Order_Details(
+    id INT order_detail_id(PRIMARY KEY),
+    FOREIGN KEY (order_id) REFERENCES Order(id),
+    quantity DOUBLE NOT NULL,
+
+)
