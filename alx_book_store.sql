df27fd22c60b547dd318a4d5b6935b4b41b7edc7
Martin Odongo <martinwere14@gmail.com>
2025-07-03 00:14:18 +0300

    library database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -27,7 +27,8 @@ CREATE Orders(
 );
 CREATE Order_Details(
     order_details INT PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY (order_id) REFERENCES Order(id),
+    FOREIGN KEY (order_id) REFERENCES Order(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE NOT NULL
 
 )
